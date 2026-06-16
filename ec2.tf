91f3a406e296616ae3613817e869062ebf07eee3
Nitin Gumber <aroranitin486@gmail.com>
2026-06-16 20:07:17 +0530

    Add ternary operators

--- ec2.tf
+++ ec2.tf
@@ -76,7 +76,7 @@ resource "aws_instance" "my_instance" {
   user_data              = file("install_nginx.sh")
 
   root_block_device {
-    volume_size = var.ec2_root_storage_size
+    volume_size = var.env == "prd" ? 20 : var.ec2_default_root_storage_size
     volume_type = "gp3"
   }
 
--- variables.tf
+++ variables.tf
@@ -3,12 +3,17 @@ variable "ec2_instance_type" {
   type    = string
 }
 
-variable "ec2_root_storage_size" {
-  default = 15
+variable "ec2_default_root_storage_size" {
+  default = 10
   type    = number
 }
 
 variable "ec2_ami_id" {
   default = "ami-07a00cf47dbbc844c"
+  type    = string
+}
+
+variable "env" {
+  default = "prd"
   type    = string
 }
